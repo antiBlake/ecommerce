d50ac0e2165fa5eca6922a2742af9c5116f353b1
Feyisayo Ibukun <ibukun209@gmail.com>
2022-07-22 06:26:30 +0100

    installed all necessary packages

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,14 @@
+import { Wrapper } from "./navbar.styles"
+
+
+
+const Navbar = () => {
+    return(
+        <Wrapper>
+
+            <nav>This is the navigation bar!!</nav>
+        </Wrapper>
+    )
+}
+
+export default Navbar
