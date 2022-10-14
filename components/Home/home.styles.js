62e0561d2dc7216c14fe3638886d9c5aa559772f
Chimaobi <ps3hdguy@gmail.com>
2022-10-14 18:35:31 +0100

    testing with alternate branch

--- components/Home/home.styles.js
+++ components/Home/home.styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   overflow-y: scroll;
   height: 100%;
-  background: white;
+  background: #fff;
   ::-webkit-scrollbar {
     display: none;
   }
