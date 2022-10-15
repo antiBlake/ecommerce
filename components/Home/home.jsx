e0cea2f164b4fe1ded0a0a78312241405fe0fd92
Feyisayo Ibukun <ibukun209@gmail.com>
2022-10-15 23:50:35 +0100

    like button partially functional

--- components/Home/home.jsx
+++ components/Home/home.jsx
@@ -13,6 +13,7 @@ import LocalMallIcon from '@mui/icons-material/LocalMall';
 import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
 
 
@@ -23,9 +24,10 @@ const Home = ({ results }) => {
   const { getCartQuantity, cartOpen, setCartOpen } = useShoppingCart();
   const { user, loading, error } = useUser()
   const [productData, setProductData] = useState(results)
+  console.log(productData)
   const [hasMore, setHasMore] = useState(true)
   const lastId = useRef(results[results.length - 1]._id)
-console.log(productData)
+  const [likes, setLikes] = useState({likeCount: 300, likeState: false})
   useEffect(() => {
 
     async function fetchData() {
@@ -39,15 +41,23 @@ console.log(productData)
           }
         })
         const final = await data.json()
-        console.log(final)
       }
     }
     fetchData()
   }, [user])
 
 
+  function handleLikes() {
+    setLikes(prev => {
+      if (prev.likeState) {
+  return {likeCount: prev.likeCount -1, likeState: false}
+      }
+      return{likeCount: prev.likeCount +1, likeState: true}
+
+    })
+  }
+
   async function fetchNextPage() {
-    console.log("this function is called")
     const { current } = lastId
     if (current === null) {
       setHasMore(false)
@@ -65,7 +75,6 @@ console.log(productData)
     if (data.length > 0) {
       lastId.current = data[data.length - 1]._id
       setProductData(prev => [...prev, ...data])
-      console.log("i did it")
     } else {
       lastId.current = null // Reached the end
       setHasMore(false)
@@ -126,13 +135,16 @@ console.log(productData)
               
               <div>
                 <div id='action-section'>
-                  <motion.div whileTap={{scale: 0.9}}>
-                    
-                  <FavoriteBorderIcon fontSize='large' sx={{marginRight: '10px'}}/>
+                  <motion.div whileTap={{ scale: 0.8 }} onClick={handleLikes}>
+                    {
+likes.likeState ?
+                        <FavoriteIcon fontSize='large' sx={{ marginRight: '10px' }} color = 'error'/>:<FavoriteBorderIcon fontSize='large' sx={{ marginRight: '10px' }} /> 
+                        
+                    }
                   </motion.div>
                   <CommentRoundedIcon fontSize='large'/>
                 </div>
-                <h4 style={{ marginTop: '10px'}}>3000 likes</h4>
+                <h4 style={{ marginTop: '10px'}}>{`${likes.likeCount} likes`}</h4>
               </div>
 
             </ProductInfo>
@@ -150,5 +162,6 @@ export default Home;
 
 export const getServerSideProps = async () => {
   const results = await sanityClient.fetch(productQuery);
+  
   return { props: { results } };
 };
