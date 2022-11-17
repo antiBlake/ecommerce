08c4b5806eded524c83ce9405855231ecb189dfb
Feyisayo Ibukun <ibukun209@gmail.com>
2022-11-17 13:09:51 +0100

    edited webhook algolia

--- pages/api/search/algoliaWebhook.js
+++ pages/api/search/algoliaWebhook.js
@@ -4,7 +4,7 @@ import { sanityClient } from "../../../lib/sanity";
 
 export default async function handler(req, res) {
   // remember to add the secret cuz this shit is hackcable I mean everything is but still. You can't make it easy for them
-
+  console.log(req.body);
   const algolia = algoliasearch(
     process.env.ALGOLIA_APP_ID,
     process.env.ALGOLIA_ADMIN_API_KEY
@@ -30,8 +30,8 @@ export default async function handler(req, res) {
   );
 
   try {
-    sanityAlgolia.webhookSync(sanityClient, { ids: req.body.ids });
-
+    sanityAlgolia.webhookSync(sanityClient, { ids: { ...req.body.ids } });
+    console.log({ ...req.body.ids });
     res.status(200).send("ok");
     return;
   } catch (err) {
