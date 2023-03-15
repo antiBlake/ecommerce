4330ff982604a0fb1b806a2f63e01bc09c5838c9
feyisayo ibukun <ibukun209@gmail.com>
2023-03-15 08:20:07 -0700

    upadated allowed sites in mangeGameAttempts to allow the wordsearch game make requests to the endpoint

--- pages/api/manageGameAttempts.js
+++ pages/api/manageGameAttempts.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   await NextCors(req, res, {
     // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
+    origin: "http://127.0.0.1:5500",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
   const { productId, numberOfAttempts } = JSON.parse(req.body);
