29f0ebcef5bd9f0a8cf62ee94ec276a5ec348490
feyisayo ibukun <ibukun209@gmail.com>
2023-03-24 23:07:44 +0100

    updated managegame attempts api

--- pages/api/manageGameAttempts.js
+++ pages/api/manageGameAttempts.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
       break;
     }
     case "PUT": {
-      // handle PUT request     console.log(req.body, "hello its me this is it");
+      console.log(req.body, "hello its me this is it");
       const { sessionId } = JSON.parse(req.body);
 
       const data = await client
