61679c1fb86751e4ed02adaf18e3437ae2c45022
antiBlake <76494602+antiBlake@users.noreply.github.com>
2023-08-08 21:44:12 +0100

    deposit page new design

--- components/profilePage/walletPage/walletPage.tsx
+++ components/profilePage/walletPage/walletPage.tsx
@@ -3,6 +3,7 @@ import { Button } from "@mui/material";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { usePaystackPayment } from "react-paystack";
+import { CloseOutlined } from "@mui/icons-material";
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 
 import {
@@ -19,6 +20,12 @@ const WalletPage = () => {
   const [userId, setUserId] = useState("");
   const [amount, setAmount] = useState("");
   console.log(user);
+
+  const [depositList, setDepositList] = useState(false)
+
+  const handleDeposit = () =>{
+    setDepositList(!depositList)
+  }
 
   useEffect(() => {
     const getUID = async () => {
@@ -93,19 +100,22 @@ const WalletPage = () => {
         />
         <Button type="submit">Add Money</Button>
       </form> */}
-      <div className="flex flex-row items-center justify-between text-2xl py-12">
-        <div className="flex flex-row items-center">
+      <div className="flex flex-col items-center justify-center text-2xl py-12 gap-y-2">
+        {/* <div className="flex flex-row items-center">
           <VisibilityOutlinedIcon />
           <h2 className="text-2xl mx-2">Total Balance:</h2>
+          </div> */}
+          <div className="text-3xl md:text-4xl font-medium">
+          ₦{amount}1500
           </div>
-          <div className="font-medium">
-          ₦{amount}
-          </div>
+          <h4 className="text-gray-500 text-sm">Available</h4>
            </div>
+
+           <div className={`${depositList ? 'h-screen top-0 left-0 absolute w-[457px] z-10 bg-black opacity-60' : ''}`} onClick={handleDeposit}></div>
 
            <div className="flex flex-row justify-between gap-x-4 mb-12">
             <div className="w-2/4">
-              <Link href='/profile/wallet/deposit'><button className="w-full bg-black text-white h-12 rounded-md">Deposit</button></Link>
+              <div onClick={handleDeposit}><button className="w-full bg-black text-white h-12 rounded-md">Deposit</button></div>
             </div>
             <div className="w-2/4">
             <Link href='/profile/wallet/withdraw'><button className="w-full bg-white text-black h-12 border border-black rounded-md">Withdraw</button></Link>
@@ -118,6 +128,24 @@ const WalletPage = () => {
       <TransactionHistoryWrapper>
         This is your transaction history
       </TransactionHistoryWrapper>
+      <div className={` ${depositList ? 'translate-y-0' : 'translate-y-full'} w-[457px] left-0 flex flex-col text-center absolute bottom-0 z-20 bg-white rounded-t-lg gap-y-6 px-4 pb-12 transition-all duration-500 ease-in-out transform-gpu`} >
+          <div className="flex flex-row justify-between items-end ">
+            <div className="mt-8 text-2xl font-medium">Select deposit method</div>
+            <div><CloseOutlined onClick={handleDeposit} className="text-2xl cursor-pointer"/></div>
+            </div>
+          <div className="flex flex-col border p-4 text-left cursor-pointer shadow-md">
+            <div className="font-medium">Card</div>
+            <div className="text-gray-400">Fund your account using debit/credit card</div>
+            </div>
+
+            <div className="flex flex-col border p-4 text-left cursor-pointer shadow-md">
+            <div className="font-medium">Bank Transfer</div>
+            <div className="text-gray-400">Fund your account using bank app</div>
+            </div>
+
+          
+  
+        </div>
     </Wrapper>
   );
 };
