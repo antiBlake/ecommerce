7c3e77a51473e8837e80d3cc314b03ff499963e9
Feyisayo Ibukun <ibukun209@gmail.com>
2023-09-19 00:45:53 +0100

    fixed bug on home page

--- components/Home/productItem/productContainer.tsx
+++ components/Home/productItem/productContainer.tsx
@@ -36,7 +36,7 @@ const ProductContainer = ({
   userSavedProducts,
 }: ProductProps) => {
   const sanityUID = useSanityUIDContext();
-  console.log("Hi mom!", sanityUID);
+
   const router = useRouter();
   const [likes, setLikes] = useState({ likeCount: 5, likeState: false });
   const [postSaveState, setPostSaveState] = useState(false);
@@ -119,123 +119,134 @@ const ProductContainer = ({
     handler();
   }, [user, userLikedProducts]);
 
-  return (
-    <ProductInfo key={productProps._id}>
-      <div id="vendor-info-container">
-        <div id="vendor-info">
-          <Image
-            placeholder="blur"
-            blurDataURL="/placeholder.png"
-            className="vendorImage"
-            width={50}
-            height={50}
-            src={urlFor(productProps.vendor.logo).url()}
-            alt={productProps.title}
-            onClick={() => {
-              router.push(`/vendor/${productProps.vendor._id}`);
-            }}
-          />
-          <span style={{ marginLeft: "30px" }}>
-            {productProps.vendor.title}
-          </span>
-        </div>
-        <Button style={{ color: "black" }}>
-          <MoreHorizIcon fontSize="large" />
-        </Button>
-      </div>
-      <motion.div
-        whileTap={{ scale: 0.9 }}
-        style={{ width: "100%", height: "20rem", position: "relative" }}
-      >
-        <Image
-          placeholder="blur"
-          blurDataURL="/placeholder.png"
-          layout="fill"
-          objectFit="cover"
-          src={urlFor(productProps.defaultProductVariant.images[0]).url()}
-          alt="Product Image"
-          onClick={() => {
-            router.push(`/product/${productProps.slug.current}`);
-          }}
-        />
-      </motion.div>
+  
+  
 
-      <div style={{ paddingLeft: "3vw" }}>
-        <div id="action-section">
-          <div>
-            <motion.button
-              whileTap={{ scale: 0.8 }}
-              onClick={() => {
-                setLikes((prev) => {
-                  if (prev.likeState) {
-                    return { likeCount: prev.likeCount - 1, likeState: false };
-                  }
-                  return { likeCount: prev.likeCount + 1, likeState: true };
-                });
-                handleLikes(productProps._id);
-              }}
-            >
-              {likes.likeState ? (
-                <FavoriteIcon
-                  fontSize="large"
-                  sx={{ marginRight: "10px" }}
-                  color="error"
-                />
-              ) : (
-                <FavoriteBorderIcon
-                  fontSize="large"
-                  sx={{ marginRight: "10px" }}
-                />
-              )}
-            </motion.button>
-            <button>
-              <CommentRoundedIcon
-                fontSize="large"
-                sx={{ marginRight: "10px" }}
+  return (
+
+    <ProductInfo key={productProps._id}>
+          <div id="vendor-info-container">
+            <div id="vendor-info">
+              <Image
+                placeholder="blur"
+                blurDataURL="/placeholder.png"
+                className="vendorImage"
+                width={50}
+                height={50}
+                src={urlFor(productProps.vendor.logo).url()}
+                alt={productProps.title}
+                onClick={() => {
+                  router.push(`/vendor/${productProps.vendor._id}`);
+                }}
               />
-            </button>
-            <motion.button
-              whileTap={{ scale: 0.8 }}
-              onClick={async () => {
-                try {
-                  await navigator.share({
-                    title: "Ecommerce",
-                    text: "BUY THIS NOW!!!",
-                    url: `/product/${productProps.slug.current}`,
-                  });
-                } catch (err) {
-                  alert(err);
-                }
-              }}
-            >
-              <SendRoundedIcon fontSize="large" sx={{ marginRight: "10px" }} />
-            </motion.button>
+              <span style={{ marginLeft: "30px" }}>
+                {productProps.vendor.title}
+              </span>
+            </div>
+            <Button style={{ color: "black" }}>
+              <MoreHorizIcon fontSize="large" />
+            </Button>
           </div>
-          <motion.button
-            id="save-button"
-            whileTap={{ scale: 0.8 }}
-            onClick={() => {
-              setPostSaveState((prev) => {
-                handlePostSave(productProps._id);
-                return !prev;
-              });
-            }}
+          <motion.div
+            whileTap={{ scale: 0.9 }}
+            style={{ width: "100%", height: "20rem", position: "relative" }}
           >
-            {postSaveState ? (
-              <BookmarkIcon fontSize="large" sx={{ marginRight: "10px" }} />
-            ) : (
-              <BookmarkBorderRoundedIcon
-                fontSize="large"
-                sx={{ marginRight: "10px" }}
-              />
-            )}
-          </motion.button>
-        </div>
-        <h4 style={{ marginTop: "10px" }}>{`${likes.likeCount} like${
-          likes.likeCount > 1 || likes.likeCount == 0 ? "s" : ""
-        }`}</h4>
-      </div>
-    </ProductInfo>
+            <Image
+              placeholder="blur"
+              blurDataURL="/placeholder.png"
+              layout="fill"
+              objectFit="cover"
+              src={urlFor(productProps.defaultProductVariant.images[0]).url()}
+              alt="Product Image"
+              onClick={() => {
+                router.push(`/product/${productProps.slug.current}`);
+              }}
+            />
+          </motion.div>
+
+          <div style={{ paddingLeft: "3vw" }}>
+            <div id="action-section">
+              <div>
+                <motion.button
+                  whileTap={{ scale: 0.8 }}
+                  onClick={() => {
+                    setLikes((prev) => {
+                      if (prev.likeState) {
+                        return {
+                          likeCount: prev.likeCount - 1,
+                          likeState: false,
+                        };
+                      }
+                      return { likeCount: prev.likeCount + 1, likeState: true };
+                    });
+                    handleLikes(productProps._id);
+                  }}
+                >
+                  {likes.likeState ? (
+                    <FavoriteIcon
+                      fontSize="large"
+                      sx={{ marginRight: "10px" }}
+                      color="error"
+                    />
+                  ) : (
+                    <FavoriteBorderIcon
+                      fontSize="large"
+                      sx={{ marginRight: "10px" }}
+                    />
+                  )}
+                </motion.button>
+                <button>
+                  <CommentRoundedIcon
+                    fontSize="large"
+                    sx={{ marginRight: "10px" }}
+                  />
+                </button>
+                <motion.button
+                  whileTap={{ scale: 0.8 }}
+                  onClick={async () => {
+                    try {
+                      await navigator.share({
+                        title: "Ecommerce",
+                        text: "BUY THIS NOW!!!",
+                        url: `/product/${productProps.slug.current}`,
+                      });
+                    } catch (err) {
+                      alert(err);
+                    }
+                  }}
+                >
+                  <SendRoundedIcon
+                    fontSize="large"
+                    sx={{ marginRight: "10px" }}
+                  />
+                </motion.button>
+              </div>
+              <motion.button
+                id="save-button"
+                whileTap={{ scale: 0.8 }}
+                onClick={() => {
+                  setPostSaveState((prev) => {
+                    handlePostSave(productProps._id);
+                    return !prev;
+                  });
+                }}
+              >
+                {postSaveState ? (
+                  <BookmarkIcon fontSize="large" sx={{ marginRight: "10px" }} />
+                ) : (
+                  <BookmarkBorderRoundedIcon
+                    fontSize="large"
+                    sx={{ marginRight: "10px" }}
+                  />
+                )}
+              </motion.button>
+            </div>
+            <h4 style={{ marginTop: "10px" }}>{`${likes.likeCount} like${
+              likes.likeCount > 1 || likes.likeCount == 0 ? "s" : ""
+            }`}</h4>
+          </div>
+        </ProductInfo>
+      
   );
 };
 
