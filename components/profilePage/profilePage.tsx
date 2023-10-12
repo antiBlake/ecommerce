74645ffba40bd31f9e67caa192b916c4488ea8c2
antiBlake <76494602+antiBlake@users.noreply.github.com>
2023-10-12 15:08:03 +0100

    Adding loading screen

--- components/profilePage/profilePage.tsx
+++ components/profilePage/profilePage.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { User } from "../../interfaces/interface";
+import { useRouter } from "next/router";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import { ProfileItem, Wrapper } from "./profilePage.styles";
@@ -9,16 +10,32 @@ import FolderSharedOutlinedIcon from "@mui/icons-material/FolderSharedOutlined";
 import HouseOutlinedIcon from '@mui/icons-material/HouseOutlined';
 import DateRangeOutlinedIcon from '@mui/icons-material/DateRangeOutlined';
 import Button from "@mui/material/Button";
-import { useRouter } from "next/router";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 import SupportAgentOutlinedIcon from '@mui/icons-material/SupportAgentOutlined';
-const ProfilePage = ({ user }: User) => {
-  const [support, setSupport] = useState(false)
 
-  const handleClick = () =>{
-    setSupport(!support)
-  }
+const ProfilePage = ({ user }: User) => {
+  const [loading, setLoading] = useState(false);
+  const [support, setSupport] = useState(false);
+  const router = useRouter();
+
+  const handleNavigation = (profileUrl: string) => {
+    setLoading(true);
+    router.push(profileUrl);
+  };
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
   const profileList = [
     {
       profileItemName: "My Details",
@@ -29,8 +46,8 @@ const ProfilePage = ({ user }: User) => {
       profileItemName: "Address book",
       profileItemIcon: <HouseOutlinedIcon />,
       profileUrl: "/profile/address",
-    },   
-     {
+    },
+    {
       profileItemName: "Wallet",
       profileItemIcon: <AccountBalanceWalletOutlinedIcon />,
       profileUrl: "/profile/wallet",
@@ -46,64 +63,89 @@ const ProfilePage = ({ user }: User) => {
       profileUrl: "/profile/membership",
     },
   ];
-  const router = useRouter();
-  // console.log(user);
+
+  const handleClick = () => {
+    setSupport(!support);
+  };
+
   return (
     <>
-    <Wrapper className="mb-16 md:mb-2">
-      <header>
-        <div id="hi">Hi,</div>
-        <div id="user-name">{user.nickname}</div>
-      </header>
-      <div className={`${support ? 'h-screen top-0 w-screen absolute z-10 backdrop-filter backdrop-blur-sm' : ''}`} onClick={handleClick}></div>
-
-      {profileList.map((item, i) => (
-        <ProfileItem
-        className="cursor-pointer"
-          key={i}
-          onClick={() => {
-            router.push(`${item.profileUrl}`);
-          }}
-        >
-          <div className="profile-name-container cursor-pointer">
-            {item.profileItemIcon}
-            <div>{item.profileItemName}</div>
+      <Wrapper className="mb-16 md:mb-2">
+        {/* Render the loading page if loading is true */}
+        {loading && (
+          <div className="loading-page h-screen top-0 w-screen absolute z-10 bg-white flex justify-center items-center">
+            <svg className="animate-spin -ml-1 mr-3 h-16 w-16 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
           </div>
-          <ArrowForwardIosRoundedIcon />
-        </ProfileItem>
-      ))}
-              <ProfileItem
-        className="cursor-pointer"
-        onClick={handleClick}
-        
-        >
+        )}
+
+        <header>
+          <div id="hi">Hi,</div>
+          <div id="user-name">{user.nickname}</div>
+        </header>
+
+        <div
+          className={`${
+            support
+              ? "h-screen top-0 w-screen absolute z-10 backdrop-filter backdrop-blur-sm"
+              : ""
+          }`}
+          onClick={handleClick}
+        ></div>
+
+        {profileList.map((item, i) => (
+          <ProfileItem
+            className="cursor-pointer"
+            key={i}
+            onClick={() => {
+              handleNavigation(item.profileUrl);
+            }}
+          >
+            <div className="profile-name-container cursor-pointer">
+              {item.profileItemIcon}
+              <div>{item.profileItemName}</div>
+            </div>
+            <ArrowForwardIosRoundedIcon />
+          </ProfileItem>
+        ))}
+
+        <ProfileItem className="cursor-pointer" onClick={handleClick}>
           <div className="profile-name-container cursor-pointer">
-          <SupportAgentOutlinedIcon />
+            <SupportAgentOutlinedIcon />
             <div>Customer support</div>
           </div>
           <ArrowForwardIosRoundedIcon />
         </ProfileItem>
 
-      <Button
-      className="text-2xl mt-2 normal-case"
-        color="error"
-        onClick={() => {
-          router.replace("/api/auth/logout");
-        }}
-      >
-        Log Out
-      </Button>
+        <Button
+          className="text-2xl mt-2 normal-case"
+          color="error"
+          onClick={() => {
+            setLoading(true); // Show loading while logging out
+            router.replace("/api/auth/logout");
+          }}
+        >
+          Log Out
+        </Button>
 
-      <div className={` ${support ? 'translate-y-0' : 'translate-y-full'} w-full flex flex-col text-center absolute bottom-0 z-20 bg-white rounded-t-lg gap-y-6  transition-all duration-500 ease-in-out transform-gpu`} >
+        <div
+          className={` ${
+            support ? "translate-y-0" : "translate-y-full"
+          } w-full flex flex-col text-center absolute bottom-0 z-20 bg-white rounded-t-lg gap-y-6 transition-all duration-500 ease-in-out transform-gpu`}
+        >
           <div className="text-sm text-gray-500 pt-2 ">Contact Us</div>
           <div className="cursor-pointer">Whatsapp</div>
           <div className="border-t pt-3 cursor-pointer ">Email</div>
-          <div className="border-t text-gray-500 mb-20 md:mb-8 pt-3 cursor-pointer " onClick={handleClick}>Cancel</div>
-  
+          <div
+            className="border-t text-gray-500 mb-20 md:mb-8 pt-3 cursor-pointer "
+            onClick={handleClick}
+          >
+            Cancel
+          </div>
         </div>
-    </Wrapper>
-
-        </>
+      </Wrapper>
+    </>
   );
 };
 
--- components/profilePage/walletPage/walletPage.tsx
+++ components/profilePage/walletPage/walletPage.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { usePaystackPayment } from "react-paystack";
 import { CloseOutlined } from "@mui/icons-material";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import dynamic from 'next/dynamic'; // Lazy loading for heavy components
 
 import {
   StoreCard,
@@ -15,51 +14,59 @@ import {
 import { useRouter } from "next/router";
 import { sanityClient } from "../../../lib/sanity";
 
-const WalletPage = () => {
+const WalletPage: React.FC = () => {
   const router = useRouter();
   const { user, error } = useUser();
-  const [userId, setUserId] = useState("");
-  const [amount, setAmount] = useState("0.00");
-  console.log(user);
+  const [userId, setUserId] = useState<string>("");
+  const [amount, setAmount] = useState<string>("0.00");
 
-  // Consolidate state into a single object
-  const [uiState, setUIState] = useState({
-    depositList: false,
-    accountdetails: false,
-    carddetails: false,
-    copied: false,
-  });
+  const [depositList, setDepositList] = useState<boolean>(false);
+  const [accountdetails, setAccountdetails] = useState<boolean>(false);
+  const [carddetails, setcarddetails] = useState<boolean>(false);
 
-  const handleToggle = (key) => {
-    setUIState((prevState) => ({ ...prevState, [key]: !prevState[key] }));
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const handleDeposit = () => {
+    setDepositList(!depositList);
+  };
+
+  const handleAccount = () => {
+    setAccountdetails(!accountdetails);
+  };
+
+  const handleCard = () => {
+    setcarddetails(!carddetails);
   };
 
   const handleCopyClick = () => {
     const contentToCopy = document.getElementById('contentToCopy');
-    const textToCopy = contentToCopy.innerText;
+    if (contentToCopy) {
+      const textToCopy = contentToCopy.innerText;
 
-    navigator.clipboard.writeText(textToCopy)
-      .then(() => {
-        handleToggle('copied');
-        setTimeout(() => handleToggle('copied'), 2000);
-      })
-      .catch(error => {
-        console.error('Error copying text:', error);
-      });
+      navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+          setCopied(true);
+          setTimeout(() => setCopied(false), 2000);
+        })
+        .catch(error => {
+          console.error('Error copying text:', error);
+        });
+    }
   };
 
   useEffect(() => {
     const getUID = async () => {
-      const data = await sanityClient.fetch(`
+      const data = await sanityClient.fetch<{ _id: string }[]>(
+        `
         *[_type == 'users' && email == $auth0ID]{
-          _id,
-          name,
-          phoneNumber,
-          address
-        }`, { auth0ID: user?.email });
+          _id
+        }`, { auth0ID: user?.email }
+      );
 
-      setUserId(data);
+      setUserId(data[0]?._id || "");
+      console.log(data)
     };
+    
 
     getUID();
   }, [user]);
@@ -72,7 +79,6 @@ const WalletPage = () => {
   const initializePayment = usePaystackPayment(config);
 
   const onSuccess = () => {
-    console.log(amount);
     alert("Your payment was successful");
     fetch("/api/addToWallet", {
       method: "POST",
@@ -85,10 +91,9 @@ const WalletPage = () => {
         if (!res.ok) {
           alert("Could not add to the wallet. Please contact dev");
           router.push("/profile/wallet");
-          return;
+        } else {
+          alert("Money was successfully added. Thank you!!!");
         }
-
-        alert("Money was successfully added. Thank you!!!");
       })
       .catch((err) => {
         console.log(err, "This didn't");
@@ -101,7 +106,137 @@ const WalletPage = () => {
 
   return (
     <Wrapper className="mt-20 mx-4">
-      {/* Your component content goes here */}
+      {/* <StoreCard>
+        <div>2000</div>
+      </StoreCard> */}
+      {/* <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          initializePayment(onSuccess, onClose);
+        }}
+      >
+        <input
+          type="number"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+          value={amount}
+        />
+        <Button type="submit">Add Money</Button>
+      </form> */}
+      <div className="flex flex-col items-center justify-center text-2xl py-12 gap-y-2">
+        {/* <div className="flex flex-row items-center">
+          <VisibilityOutlinedIcon />
+          <h2 className="text-2xl mx-2">Total Balance:</h2>
+          </div> */}
+          <div className="text-3xl md:text-4xl font-medium">
+          ₦{amount}
+          </div>
+          <h4 className="text-gray-500 text-sm">Available</h4>
+           </div>
+
+           <div className={`${depositList ? 'h-screen top-0 left-0 absolute w-[457px] z-10 bg-black opacity-60' : ''}`} onClick={handleDeposit}></div>
+
+           <div className="flex flex-row justify-between gap-x-4 mb-12">
+            <div className="w-2/4">
+              <div onClick={handleDeposit}><button className="w-full bg-black text-white h-12 rounded-md">Deposit</button></div>
+            </div>
+            <div className="w-2/4">
+            <Link href='/profile/wallet/withdraw'><button className="w-full bg-white text-black h-12 border border-black rounded-md">Withdraw</button></Link>
+
+            </div>
+
+
+           </div>
+
+      <TransactionHistoryWrapper>
+        This is your transaction history
+      </TransactionHistoryWrapper>
+      <div className={` ${depositList ? 'translate-y-0' : 'translate-y-full'} w-full left-0 flex flex-col text-center absolute bottom-0 z-20 bg-white rounded-t-lg gap-y-6 px-4 pb-12 transition-all duration-500 ease-in-out transform-gpu`} >
+          <div className="flex flex-row justify-between items-end ">
+            <div className="mt-8 text-2xl font-medium">Select deposit method</div>
+            <div><CloseOutlined onClick={handleDeposit} className="text-2xl mb-2 cursor-pointer"/></div>
+            </div>
+          
+
+            <div className="flex flex-col border p-4 text-left cursor-pointer shadow-md" onClick={handleCard}>
+            <div className="font-medium">Card</div>
+            <div className="text-gray-400">Fund your account using debit/credit card</div>
+            </div>
+            
+
+            <div className="flex flex-col border p-4 text-left cursor-pointer shadow-md" onClick={handleAccount}>
+            <div className="font-medium">Bank Transfer</div>
+            <div className="text-gray-400" >Fund your account using bank app</div>
+            </div>
+
+          
+  
+        </div>
+
+        
+
+          
+        <div className={` ${accountdetails ? 'translate-y-0' : 'translate-y-full'} w-full left-0 flex flex-col text-center absolute bottom-0 z-20 bg-white rounded-t-lg gap-y-6 px-4 pb-12 transition-all duration-500 ease-in-out transform-gpu`} >
+          <div className="flex flex-row justify-between items-end ">
+            <div className="mt-8 text-2xl font-medium">Account details</div>
+            <div><CloseOutlined onClick={handleAccount} className="text-2xl mb-2 cursor-pointer"/></div>
+            </div>
+            <div className="text-left font-medium">Make payment into the account below</div>
+          <div className="flex flex-col border p-4 text-left cursor-pointer shadow-md">
+            <div className="font-medium">Account Name</div>
+            <div className="text-gray-400">Ogbonna Chimaobi</div>
+            </div>
+
+            <div className="flex flex-row border p-4 text-left items-center justify-between cursor-pointer shadow-md">
+              <div className="flex flex-col text-left">
+            <div className="font-medium">Account Number</div>
+            <div className="text-gray-400" id="contentToCopy">617 XXX 8052</div>
+             </div>
+
+             <div className="text-gray-700" onClick={handleCopyClick}><ContentCopyIcon />
+             {copied ? 'Copied!' : ''}</div>
+            </div>
+
+            <div className="flex flex-col border p-4 text-left cursor-pointer shadow-md">
+            <div className="font-medium">Bank Name</div>
+            <div className="text-gray-400">Fidelity bank</div>
+            </div>
+
+          
+  
+        </div>
+
+        <div className={` ${carddetails ? 'translate-y-0' : 'translate-y-full'} w-full left-0 flex flex-col text-center absolute bottom-0 z-20 bg-white rounded-t-lg gap-y-6 px-4 pb-12 transition-all duration-500 ease-in-out transform-gpu`} >
+          <div className="flex flex-row justify-between items-end ">
+            <div className="mt-8 text-2xl font-medium">Input Top Up Amount</div>
+            <div><CloseOutlined onClick={handleCard} className="text-2xl mb-2 cursor-pointer"/></div>
+            </div>
+          <div className="flex flex-col text-left w-full">
+          <form
+          className=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          initializePayment(onSuccess, onClose);
+        }}
+      >
+          <input type='number' placeholder='Amount (NGN)' className='h-16 w-full pl-4 placeholder-gray-800 border border-gray-800 text-lg my-8'
+          onChange={(e) => {
+            setAmount(e.target.value)
+          }}
+          value={amount}
+          />
+            
+          <button type="submit" className='w-full h-16 text-white bg-black rounded-md'>Top Up Now</button>
+
+          </form>
+            </div>
+
+          
+  
+        </div>
+
+        
     </Wrapper>
   );
 };
