709745925d2823cf5db1254f5f96308bb427d816
Feyisayo Ibukun <ibukun209@gmail.com>
2023-10-22 20:25:48 +0100

    save functionality bug fixes

--- pages/api/handlePostSave.js
+++ pages/api/handlePostSave.js
@@ -5,30 +5,41 @@ const client = sanity(config);
 
 export default async function handler(req, res) {
   console.log(req.body);
-  const { userId, productId, saveState } = JSON.parse(req.body);
-  console.log(likeState, "this is the like state");
+  const { userId, productId } = JSON.parse(req.body);
+
+  let userDoc = await client.getDocument(userId);
+
+  // check if product id is already in liked products array
+
+  let saveState = userDoc.savedProducts.find(
+    (product) => product._ref == productId
+  );
+
   if (saveState) {
-    client
-      .patch(userId)
-      .append("savedProducts", [productId])
-      .commit({
-        // Adds a `_key` attribute to array items, unique within the array, to
-        // ensure it can be addressed uniquely in a real-time collaboration context
-        autoGenerateArrayKeys: true,
-      })
-      .catch((err) => {
-        console.error("Oh no, the update failed: ", err.message);
-        res.status(500).json({ message: "Yh, that didnt work bro" });
-      });
+    try {
+      await client
+        .patch(userId)
+        .setIfMissing({ savedProducts: [] })
+        .append("savedProducts", [productId])
+        .commit({
+          // Adds a `_key` attribute to array items, unique within the array, to
+          // ensure it can be addressed uniquely in a real-time collaboration context
+          autoGenerateArrayKeys: true,
+        });
+    } catch (err) {
+      console.error("Oh no, the update failed: ", err.message);
+      res.status(500).json({ message: "Yh, that didnt work bro" });
+    }
   } else {
-    client
-      .patch(userId)
-      .unset([`savedProducts[_key=="${productItemKey}"]`])
-      .commit()
-      .catch((err) => {
-        console.error("Oh no, the update failed: ", err.message);
-        res.status(500).json({ message: "Yh, that didnt work bro" });
-      });
+    try {
+      await client
+        .patch(userId)
+        .unset([`savedProducts[_key=="${saveState._key}"]`])
+        .commit();
+    } catch (err) {
+      console.error("Oh no, the update failed: ", err.message);
+      res.status(500).json({ message: "Yh, that didnt work bro" });
+    }
   }
 
   res.status(200).send({ message: "that worked!!" });
