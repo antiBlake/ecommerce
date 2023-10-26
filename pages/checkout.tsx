daa06b2fe92f2e303632a7024b8adfb49fc1b2f8
David <doladepo128@gmail.com>
2023-10-26 12:03:21 +0100

    small changes

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { UserProfile, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { User } from "../interfaces/interface";
 import productcheckout from "./Itemcheckout";
-//import CheckoutPage from "../components/Checkout/checkoutPage";
+
 
 const Checkout = ({ user }: User) => {
   return  null;
